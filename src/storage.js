86284cad56b0918699db501ff4706aebd99e6627
Askar Yusupov <devex.soft@gmail.com>
2016-12-03 16:56:11 +0500

    added sync storage

--- src/storage.js
+++ src/storage.js
@@ -1,29 +1,31 @@
-let storage = chrome.storage.local;
+function createStorage(storage) {
+  storage._lastData = null; // Deal with Chrome issue https://code.google.com/p/chromium/issues/detail?id=361113
 
-storage._lastData = null; // Deal with Chrome issue https://code.google.com/p/chromium/issues/detail?id=361113
+  storage.getItem = (key, callback) => {
+    storage.get(key, obj => {
+      if (obj[key]) callback(null, obj[key]);
+      else callback(chrome.runtime.lastError, null);
+    });
+  };
 
-storage.getItem = (key, callback) => {
-  chrome.storage.local.get(key, obj => {
-    if (obj[key]) callback(null, obj[key]);
-    else callback(chrome.runtime.lastError, null);
-  });
-};
+  storage.setItem = (key, value, callback) => {
+    let obj = {};
+    obj[key] = value;
+    storage.set(obj, () => {
+      if (chrome.runtime.lastError) callback(key);
+    });
+    storage._lastData = value;
+  };
 
-storage.setItem = (key, value, callback) => {
-  let obj = {};
-  obj[key] = value;
-  chrome.storage.local.set(obj, () => {
-    if (chrome.runtime.lastError) callback(key);
-  });
-  storage._lastData = value;
-};
+  storage.removeItem = storage.remove;
 
-storage.removeItem = storage.remove;
+  storage.getAllKeys = callback => {
+    storage.get(null, obj => {
+      callback(null, Object.keys(obj));
+    });
+  };
+}
 
-storage.getAllKeys = callback => {
-  chrome.storage.local.get(null, obj => {
-    callback(null, Object.keys(obj));
-  });
-};
-
-export default storage;
+export const local = createStorage(chrome.storage.local);
+export const sync = createStorage(chrome.storage.sync);
+export default local;
