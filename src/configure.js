f5bf7a0d05deace4118bc53b47c3ea34ca6a4e72
Askar Yusupov <devex.soft@gmail.com>
2016-12-05 08:45:30 +0500

    fixed configureSync storage pass

--- src/configure.js
+++ src/configure.js
@@ -3,7 +3,7 @@ import storage from './storage';
 export default function configureSync(config = {}) {
   return  {
     ...config,
-    storage: storage,
+    storage: config.storage || storage,
     createPersistor: true,
     serialize: data => data,
     deserialize: data => data,
