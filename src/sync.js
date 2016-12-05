27d278f1e9bfa6447d87bfaadcf25e61e82d9f06
Askar Yusupov <devex.soft@gmail.com>
2016-12-05 08:51:31 +0500

    fixed sync

--- src/sync.js
+++ src/sync.js
@@ -6,12 +6,13 @@ function sync(persistor, config){
   if (config === undefined) config = {};
   var blacklist = config.blacklist || false;
   var whitelist = config.whitelist || false;
+  var storage = config.storage || throw new Error('missing "storage" property in config parameter');
 
   chrome.storage.onChanged.addListener(handleStorageEvent);
 
   function handleStorageEvent(e){
     var key = Object.keys(e)[0];
-    if(key && key.indexOf(keyPrefix) === 0 && (!chrome.storage.local._lastData || !isEqual(e[key].newValue, chrome.storage.local._lastData))){
+    if(key && key.indexOf(keyPrefix) === 0 && (!storage._lastData || !isEqual(e[key].newValue, storage._lastData))){
       var keyspace = key.substr(keyPrefix.length);
       if(whitelist && whitelist.indexOf(keyspace) === -1){ return }
       if(blacklist && blacklist.indexOf(keyspace) !== -1){ return }
